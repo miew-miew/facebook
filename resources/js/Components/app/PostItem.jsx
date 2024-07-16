df8bbc76c9219272a06315236292138010859f6c
miew-miew <nrzanakoto011@gmail.com>
2024-07-16 08:43:45 +0200

    WIP: Setting up homepage

--- resources/js/Components/app/PostItem.jsx
+++ resources/js/Components/app/PostItem.jsx
@@ -1,47 +1,84 @@
 import { Disclosure } from "@headlessui/react";
-import { ChevronRightIcon } from "@heroicons/react/24/solid";
+import { ArrowDownTrayIcon, ChevronRightIcon, DocumentIcon } from "@heroicons/react/24/solid";
 import { Link } from "@inertiajs/react";
+
+function isImage(attachement){
+    const mime = attachement.mime.split('/')
+    return mime[0].toLowerCase() === 'image'
+}
 
 export default function PostItem({ post }) {
     return(
         <div className="bg-white border rounded p-4 mb-3">
             <div className="flex items-center gap-2 mb-3">
                 <Link>
-                    <img src={post.user.avatar} className="w-[40px] rounded-full" alt="User Avatar" />
+                    <img src={post.user.avatar} className="w-[40px] rounded-full border-2 hover:border-blue-500" />
                 </Link>
-                <div className="flex items-center">
-                    <h4 className="font-bold mr-1">
-                        <Link>
-                            {post.user.name}
-                        </Link>
-                    </h4>
-                    { post.group && (
-                        <div className="flex items-center">
-                            <ChevronRightIcon className="size-4 text-gray-900" />
-                            <h4 className="font-bold mr-1">
-                                <Link>
-                                    {post.group.name}
-                                </Link>
-                            </h4>
-                        </div>
-                    )}
+                <div>
+                    <div className="flex items-center">
+                        <h4 className="font-bold mr-1 hover:underline">
+                            <Link>
+                                {post.user.name}
+                            </Link>
+                        </h4>
+                        { post.group && (
+                            <div className="flex items-center">
+                                <ChevronRightIcon className="w-4 h-4 text-gray-900" />
+                                <h4 className="font-bold mr-1 hover:underline">
+                                    <Link>
+                                        {post.group.name}
+                                    </Link>
+                                </h4>
+                            </div>
+                        )}
+                    </div>
+                    <small className="text-gray-400">{post.created_at}</small>
                 </div>
             </div>
-            <Disclosure>
-            {({ open }) => (
-                <div>
-                    {!open && <div dangerouslySetInnerHTML={{ __html: post.body.substring(0, 200) }} />}
-                    <Disclosure.Panel>
-                        <div dangerouslySetInnerHTML={{ __html: post.body }} />
-                    </Disclosure.Panel>
-                    <div className="flex justify-end">
-                        <Disclosure.Button className="text-blue-500">
-                            {open ? 'Read less' : 'Read more'}
-                        </Disclosure.Button>
+            <div className="mb-3">
+                <Disclosure>
+                {({ open }) => (
+                    <div>
+                        {!open && <div dangerouslySetInnerHTML={{ __html: post.body.substring(0, 200) }} />}
+                        <Disclosure.Panel>
+                            <div dangerouslySetInnerHTML={{ __html: post.body }} />
+                        </Disclosure.Panel>
+                        <div className="flex justify-end">
+                            <Disclosure.Button className="text-blue-500 hover:underline">
+                                {open ? 'Read less' : 'Read more'}
+                            </Disclosure.Button>
+                        </div>
                     </div>
-                </div>
-            )}
-            </Disclosure>
+                )}
+                </Disclosure>
+            </div>
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-3">
+                {post.attachements && post.attachements.map((attachement) => (
+                    <div key={attachement.id} >
+                            <div className="bg-blue-100 aspect-square flex flex-col items-center justify-center text-gray-500 relative">
+                                <button className="w-8 h-8 flex items-center justify-center text-gray-100 bg-gray-700 rounded absolute right-2 top-2 cursor-pointer hover:bg-gray-800">
+                                    <ArrowDownTrayIcon className="w-4 h-4"/>
+                                </button>
+                                {isImage(attachement) ? (
+                                    <img src={attachement.url} className="object-cover aspect-square" />
+                                ) : (
+                                    <div>
+                                        <DocumentIcon className="w-16 h-16"/>
+                                        {attachement.name}
+                                    </div>
+                                )}
+                            </div>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <button>
+                    Like
+                </button>
+                <button>
+                    Comment
+                </button>
+            </div>
         </div>
     )
 }
--- resources/js/Components/app/PostList.jsx
+++ resources/js/Components/app/PostList.jsx
@@ -11,6 +11,27 @@ export default function PostList() {
 
         group: null,
 
+        attachements: [
+            {
+                id: 1,
+                name: 'test.png',
+                url: 'https://randompicturegenerator.com/img/cat-generator/g1f53aa3c8a36ccb11124d36c23c73fdf8adedaa793cc4fb1a5ca6936c10940e30ffe042636433fa4d0c353496a050f12_640.jpg',
+                mime: 'image/png' 
+            },
+            {
+                id: 2,
+                name: 'test2.png',
+                url: 'https://randompicturegenerator.com/img/dog-generator/g3b4f2b51dc698d91ce817fd5f4483c2ebedc93a0e8f26980d205d7dd06648f196c8eb3233e76166fd31c610b1dab94de_640.jpg',
+                mime: 'image/png' 
+            },
+            {
+                id: 3,
+                name: 'test.docx',
+                url: '#',
+                mime: 'application/msword' 
+            }
+        ],
+
         body: "<p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse provident quidem accusantium ipsum placeat saepe blanditiis facere amet id quae quo eligendi nisi sit similique dignissimos repellendus sequi, quaerat exercitationem assumenda? Quo molestiae atque magni provident obcaecati, vel officia beatae autem, molestias consectetur aut expedita nesciunt quam iusto, dolorum asperiores. </p>",
 
         created_at: '2024-07-15 10:54'
