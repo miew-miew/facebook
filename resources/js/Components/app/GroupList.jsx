8f48bfac8f426ee0c469a1c36b60f4a318157678
miew-miew <nrzanakoto011@gmail.com>
2024-07-21 16:57:49 +0200

    WIP: Setting up homepage

--- resources/js/Components/app/GroupList.jsx
+++ resources/js/Components/app/GroupList.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import TextInput from "../TextInput";
-import GroupItem from "./GroupItem";
 import { Disclosure } from "@headlessui/react";
 import { ChevronRightIcon } from "@heroicons/react/24/solid";
+import GroupListItems from "./GroupListItems";
 
 export default function GroupList(){
-
-    const [search, setSearch] = useState('')
     return(
         <div className="px-3 bg-white rounded border py-3 h-full overflow-hidden flex flex-col">
             <Disclosure className="flex-1 flex flex-col lg:hidden">
@@ -19,65 +15,7 @@ export default function GroupList(){
                         </div>
                     </Disclosure.Button>
                     <Disclosure.Panel>
-                        <TextInput placeholder="Type to search" value={search} className="w-full mt-4" onChange={(e) => setSearch(e.target.value)}/>
-                        <div className="mt-3 h-[200px] lg:flex-1 overflow-auto">
-                            { false ? (
-                                <div className='text-gray-400 text-center p-3'>
-                                    You are not joined to any groups
-                                </div>
-                            ) : (
-                                <div>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="Laravel Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="React.js Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="Laravel Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="React.js Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="Laravel Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="React.js Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="Laravel Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="React.js Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="Laravel Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="React.js Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="Laravel Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                    <GroupItem 
-                                        image="https://picsum.photos/100" 
-                                        title="React.js Developer" 
-                                        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                                </div>
-                            )}
-                        </div>
+                        <GroupListItems/>
                     </Disclosure.Panel>
                 </div>
             )}
@@ -85,65 +23,7 @@ export default function GroupList(){
 
             <div className="hidden max-h-screen lg:flex flex-col overflow-hidden">
                 <h2 className='text-xl font-bold'>Groups</h2>
-                <TextInput placeholder="Type to search" value={search} className="w-full mt-4" onChange={(e) => setSearch(e.target.value)}/>
-                <div className="mt-3 lg:flex-1 overflow-auto">
-                    { false ? (
-                        <div className='text-gray-400 text-center p-3'>
-                            You are not joined to any groups
-                        </div>
-                    ) : (
-                        <div>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="Laravel Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="React.js Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="Laravel Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="React.js Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="Laravel Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="React.js Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="Laravel Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="React.js Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="Laravel Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="React.js Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="Laravel Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                            <GroupItem 
-                                image="https://picsum.photos/100" 
-                                title="React.js Developer" 
-                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
-                        </div>
-                    )}
-                </div>
+                <GroupListItems/>
             </div>
         </div>
     )
---
+++ resources/js/Components/app/GroupListItems.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import TextInput from "../TextInput";
+import GroupItem from "./GroupItem";
+
+export default function GroupListItems () {
+
+    const [search, setSearch] = useState('')
+    return (
+        <div className="hidden max-h-screen lg:flex flex-col overflow-hidden">
+            <TextInput placeholder="Type to search" value={search} className="w-full mt-4" onChange={(e) => setSearch(e.target.value)}/>
+            <div className="mt-3 h-[200px] lg:flex-1 overflow-auto">
+                { false ? (
+                    <div className='text-gray-400 text-center p-3'>
+                        You are not joined to any groups
+                    </div>
+                ) : (
+                    <div>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="Laravel Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="React.js Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="Laravel Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="React.js Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="Laravel Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="React.js Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="Laravel Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="React.js Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="Laravel Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="React.js Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="Laravel Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                        <GroupItem 
+                            image="https://picsum.photos/100" 
+                            title="React.js Developer" 
+                            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."/>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
