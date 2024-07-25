cb86f342577ccfb61f9623db5d35e56bcbcaa5f6
miew-miew <nrzanakoto011@gmail.com>
2024-07-25 10:41:52 +0200

    WIP: Post creation

--- resources/js/Components/TextareaInput.jsx
+++ resources/js/Components/TextareaInput.jsx
@@ -1,4 +1,4 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useRef, useState } from 'react';
 
 export default forwardRef(function TextareaInput({ className = '', value, onChange, placeholder, isFocused = false, ...props }, ref) {
     const input = ref ? ref : useRef();
@@ -11,10 +11,12 @@ export default forwardRef(function TextareaInput({ className = '', value, onChan
 
     const onInputChange = (e) => {
         onChange(e.target.value);
+        input.current.style.height = 'auto';
+        input.current.style.height = input.current.scrollHeight + 'px';
     };
 
     return (
-        <input
+        <textarea
             {...props}
             className={
                 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
